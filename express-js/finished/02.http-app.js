d9396b4c764c377890aa07db110e6db00a7db8ad
KRHarisha <105624826+KRHarisha@users.noreply.github.com>
2022-06-27 17:41:59 +0530

    Add files via upload

--- express-js/finished/02.http-app.js
+++ express-js/finished/02.http-app.js
@@ -1,19 +1,17 @@
 const http = require("http");
+const { readFileSync } = require("fs");
 
+const homePage = readFileSync("./methods-public/index.html");
 const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  //   res.writeHead(200, { "Content-Type": "text/html" });
-  //   res.write("<h1>Hello World</h1>");
-  //   res.end();
   const url = req.url;
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>.................home page...............</h1>");
+    res.write(homePage);
     res.end();
   }
   // about page
   else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
+    res.writeHead(200, { "content-type": "text/plain" });
     res.write("<h1>..............About page..............</h1>");
     res.end();
   }
@@ -28,4 +26,3 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
--- express-js/finished/03.http-styles.js
+++ express-js/finished/03.http-styles.js
@@ -1,20 +1,29 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  //   res.writeHead(200, { "Content-Type": "text/html" });
-  //   res.write("<h1>Hello World</h1>");
-  //   res.end();
   const url = req.url;
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>.................home page...............</h1>");
+    res.write(homePage);
     res.end();
   }
   // about page
   else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
+    res.writeHead(200, { "content-type": "text/plain" });
     res.write("<h1>..............About page..............</h1>");
+    res.end();
+  } else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  } else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
     res.end();
   }
   // 404 page not found
@@ -28,4 +37,3 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
